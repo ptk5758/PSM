65c15756c3e4845f1d649513e531df7dd38b82f3
ptk5758 <ptk725739@gmail.com>
2026-06-30 23:29:46 +0900

    [style] css file 위치 수정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import { MainBoard } from '../widgets/main-board'
 import { SideBar } from '../widgets/side-bar'
 
-import './styles/reset.css'
-import './styles/root.css'
-import './styles/app.css'
+import '../assets/styles/reset.css'
+import '../assets/styles/root.css'
+import '../assets/styles/app.css'
 import WorkspaceProvider from './providers/story/WorkspaceProvider'
 
 function App() {
