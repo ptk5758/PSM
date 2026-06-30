3013acb94ac40928a7c9095cb564a461cabb29ac
ptk5758 <ptk725739@gmail.com>
2026-06-30 23:46:39 +0900

    [example] react-flow node & edge 생성 예시

--- src/widgets/main-board/ui/MainBoard.tsx
+++ src/widgets/main-board/ui/MainBoard.tsx
@@ -2,6 +2,37 @@ import style from './MainBoard.module.css'
 import { useWorkspace } from '@/app/hooks'
 import { StoryHeader } from '@/entities/story'
 import clsx from 'clsx'
+import { ReactFlow, Background, Controls, type Node, type Edge } from '@xyflow/react'
+import '@xyflow/react/dist/style.css'
+
+const initNode: Node[] = [
+    {
+        id: 'node_1',
+        data: {
+            foo: 'bar',
+            label: 'node_1'
+        },
+        position: { x: 0, y: 0 },
+    },
+    {
+        id: 'node_2',
+        data: {
+            foo: 'pong',
+            label: 'node_2'
+        },
+        position: { x: 100, y: 100 },
+    },
+]
+
+const initEdge: Edge[] = [
+    {
+        id: 'edge_1',
+        source: 'node_1',
+        target: 'node_2',
+        type: 'smoothstep',
+        label: "Edge Label label"
+    }
+]
 
 function MainBoard() {
     const { story } = useWorkspace()
@@ -13,6 +44,12 @@ function MainBoard() {
     return (
         <main className={style.main_board}>
             <StoryHeader className={style.top} {...story} />
+            <div style={{ flex: 1 }}>
+                <ReactFlow nodes={initNode} edges={initEdge}>
+                    <Background />
+                    <Controls />
+                </ReactFlow>
+            </div>
         </main>
     )
 }
